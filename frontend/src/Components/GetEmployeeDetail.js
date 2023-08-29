04179e5635cbb86d5a1d2815a4a0c42fdc920eb7
Vincent Low <vincent.low@tokopedia.com>
2023-08-29 09:38:26 +0700

    fix EmployeeDetail modal

--- frontend/src/Components/GetEmployeeDetail.js
+++ frontend/src/Components/GetEmployeeDetail.js
@@ -5,7 +5,7 @@ import {GET_EMPLOYEE_DETAIL} from '../GraphQL/Queries'
 
 function GetEmployeeDetail({employeeId}) {
     const [employee, setEmployee] = useState({})
-    const [getEmployee, {error, loading, data}] = useLazyQuery(GET_EMPLOYEE_DETAIL)
+    var [getEmployee, {error, loading, data}] = useLazyQuery(GET_EMPLOYEE_DETAIL)
 
     const handleGetEmployeeDetail = () => {
         getEmployee({
@@ -19,25 +19,29 @@ function GetEmployeeDetail({employeeId}) {
         }
     }
 
+    useEffect(() => {
+        if (data) {
+          setEmployee(data.getEmployeeDetail);
+        }
+      }, [data]);
+    
+
+    const handleRefresh = () => {
+        window.location.reload()
+    }
+
     return (
         <div>
-            <button type="button" className="btn btn-info"
-                    data-bs-toggle="modal"
-                    data-bs-target={`#emp-${employeeId}ViewModal`}
-                    onClick={() => handleGetEmployeeDetail(employeeId)}>
-                    View
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#emp-${employeeId}-view`}
+                onClick={() => handleGetEmployeeDetail(employeeId)}>
+            View
             </button>
-
-            {/* View Modal */}
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error.message}</p>}
-            {data && data.getEmployee && (
-                <div className="modal fade" id={`emp-${employeeId}ViewModal`} tabindex="-1" aria-labelledby={`emp-${employeeId}ViewModalLabel`} aria-hidden="true">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
+            <div class="modal fade" id={`emp-${employeeId}-view`}>
+                <div class="modal-dialog">
+                    <div class="modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5" id={`emp-${employeeId}ViewModalLabel`}>{employeeId}</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleRefresh}></button>
                         </div>
                         <div className="modal-body">
                             <div className="container mt-5">
@@ -56,10 +60,9 @@ function GetEmployeeDetail({employeeId}) {
                                 </table>
                             </div>
                         </div>
-                        </div>
                     </div>
                 </div>
-            )}
+            </div>
         </div>
     )
 }
